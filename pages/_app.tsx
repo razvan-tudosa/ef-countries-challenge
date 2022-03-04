bbb4b591c6ede2a90617df0e2984c3cf07f40103
razvant <tudosa.razvan@gmail.com>
2022-03-04 21:13:33 +0200

    feat: link global styles with the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { Layout } from "../components/Layout";
 import { getTheme, ThemeType } from "../config/theme";
+import { GlobalStyles } from "../styles/globalStyles";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={getTheme(ThemeType.dark)}>
+      <GlobalStyles />
       <Layout>
         <Component {...pageProps} />
       </Layout>
