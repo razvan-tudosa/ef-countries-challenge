1b735d8382c24ec2b67055c8979c881ff0d29617
razvant <tudosa.razvan@gmail.com>
2022-03-04 22:00:42 +0200

    feat: add country service api for data fetching

---
+++ services/countryService/index.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosInstance, AxiosStatic } from "axios";
+
+type Country = unknown; // TODO: create a proper type for this
+
+interface CountryServiceI {
+  getAll: () => Promise<Country[]>;
+  getOneByName: (name: string) => Promise<Country>;
+}
+
+export class CountryService implements CountryServiceI {
+  private countryApi: AxiosInstance;
+  private timeout = 1000;
+  private baseUrl = `https://restcountries.com/v3.1`;
+
+  constructor(axios: AxiosStatic) {
+    this.countryApi = axios.create({
+      baseURL: this.baseUrl,
+      timeout: this.timeout,
+    });
+  }
+
+  async getAll() {
+    const response = await this.countryApi.get("/all");
+
+    return response.data;
+  }
+
+  async getOneByName(countryName: string) {
+    const response = await this.countryApi.get(`/name/${countryName}`);
+
+    return response.data;
+  }
+}
+
+export default new CountryService(axios);
