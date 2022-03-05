169af2c4e1e8819196f495cc086635097d93c8d7
razvant <tudosa.razvan@gmail.com>
2022-03-05 18:47:17 +0200

    feat: add search input and dropdown for region filtering

--- components/Filters/index.tsx
+++ components/Filters/index.tsx
@@ -1,4 +1,40 @@
 import React from "react";
+import { Input, Dropdown, Menu } from "semantic-ui-react";
+import { Search as SearchIcon } from "@styled-icons/material/Search";
 import { Wrapper } from "./styles";
 
-export const Filters: React.FC = () => <Wrapper>Filters go here...</Wrapper>;
+const enum Regions {
+  Africa = "Africa",
+  America = "America",
+  Asia = "Asia",
+  Europe = "Europe",
+  Oceania = "Oceania",
+}
+
+type Option = {
+  key: number;
+  text: keyof typeof Regions;
+  value: keyof typeof Regions;
+};
+
+const options: Option[] = [
+  { key: 1, text: Regions.Africa, value: Regions.Africa },
+  { key: 2, text: Regions.America, value: Regions.America },
+  { key: 3, text: Regions.Asia, value: Regions.Asia },
+  { key: 4, text: Regions.Europe, value: Regions.Europe },
+  { key: 5, text: Regions.Oceania, value: Regions.Oceania },
+];
+
+export const Filters: React.FC = () => (
+  <Wrapper>
+    <Input
+      placeholder="Search for a country..."
+      icon={<SearchIcon size={20} />}
+      iconPosition="left"
+    />
+
+    <Menu compact>
+      <Dropdown text="Fiter by Region" options={options} simple item />
+    </Menu>
+  </Wrapper>
+);
--- components/Filters/styles.ts
+++ components/Filters/styles.ts
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  padding: ${({ theme }) => theme.sizes.lg};
+  padding: ${({ theme }) => theme.sizes.lg} 0;
+  display: flex;
+  justify-content: space-between;
+
+  .input {
+    svg {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      left: 10px;
+    }
+  }
 `;
