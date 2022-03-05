644c3c5efab5b4316a8595dc369c6570cc2c3e14
razvant <tudosa.razvan@gmail.com>
2022-03-05 18:45:57 +0200

    style: add styles for semantic ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import { Layout } from "../components/Layout";
 import ThemeManager from "../config/theme/ThemeManager";
 import { GlobalStyles } from "../styles/globalStyles";
 
+import "semantic-ui-css/semantic.min.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeManager>
