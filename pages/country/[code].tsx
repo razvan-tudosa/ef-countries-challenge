9dcd560d6645c27adbfcdd9ebbcdfb05ab00c145
razvant <tudosa.razvan@gmail.com>
2022-03-05 13:44:00 +0200

    feat: setup basic structure for contry details page

--- pages/country/[code].tsx
+++ pages/country/[code].tsx
@@ -6,6 +6,17 @@ import type {
 import { countryService } from "../../services";
 import type { Country } from "../../services";
 import { Content } from "../styles";
+import {
+  Col,
+  Row,
+  Details,
+  Flag,
+  Info,
+  Item,
+  Label,
+  Navigation,
+  Title,
+} from "./styles";
 
 export const getServerSideProps: GetServerSideProps<
   CountryPageProps,
@@ -25,9 +36,63 @@ interface CountryPageProps {
   country: Country;
 }
 
+type InfoRow = {
+  label: string;
+  value: string | number;
+};
+
+type Info = {
+  leftSide: InfoRow[];
+  rightSide: InfoRow[];
+};
+
 const Country: NextPage<CountryPageProps> = ({ country }) => {
   console.log(country);
-  return <Content>Country Page - Countries Details</Content>;
+
+  const info: Info = {
+    leftSide: [
+      { label: "Native Name:", value: "country.name.nativeName.cnr.common" },
+      { label: "Population:", value: country.population },
+      { label: "Region:", value: country.region },
+      { label: "Sub Region:", value: country.subregion },
+      { label: "Capital:", value: 'country.capital.join(", ")' },
+    ],
+    rightSide: [
+      { label: "Top Level Domain:", value: country.tld },
+      { label: "Currencies", value: "TBD" },
+      { label: "Languages", value: "TBD" },
+    ],
+  };
+
+  const renderColInfo = (info: InfoRow[]) =>
+    info.map(({ label, value }: InfoRow) => (
+      <Col key={value}>
+        <Item>
+          <Label>{label}</Label>
+          <span>{value}</span>
+        </Item>
+      </Col>
+    ));
+
+  return (
+    <Content>
+      Country Page - Countries Details - Country Name: {country.name.common}
+      <Navigation>
+        <button>Back</button>
+      </Navigation>
+      <Info>
+        <Flag src={country.flags.png} />
+        <Details>
+          <Title>{country.name.common}</Title>
+          <Row>
+            {renderColInfo(info.leftSide)}
+            {renderColInfo(info.rightSide)}
+            <Col></Col>
+          </Row>
+        </Details>
+      </Info>
+    </Content>
+  );
 };
 
 export default Country;
---
+++ pages/country/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Navigation = styled.div``;
+export const Info = styled.div``;
+export const Flag = styled.img``;
+export const Details = styled.div``;
+export const Title = styled.div``;
+export const Col = styled.div``;
+export const Row = styled.div``;
+export const Item = styled.div``;
+export const Label = styled.span``;
